c4ce87b09414d254eaf029b05e24f76540f2c282
Pliman <sunbin@nhn.com>
2013-07-29 09:29:25 +0800

    modify configuration.

--- etc/conf.js
+++ etc/conf.js
@@ -15,8 +15,8 @@ module.exports = {
 		}
 	},
 	"mongoDB": {
-		//"host": "10.34.130.130",
-		"host": "10.34.135.159",
+		"host": "10.34.130.130",
+		//"host": "192.168.1.108",
 		"port": "27017",
 		"dbName": "attendee",
 		"max_connection": "10",
